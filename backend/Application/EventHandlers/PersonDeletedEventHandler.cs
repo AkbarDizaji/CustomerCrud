7a1bb0fd890426588ca2da8adddf06e3373ad9b7
Akbar Dizaji <vatantech724@gmail.com>
2022-12-12 09:47:48 -0800

    add EventHandler to application layer

---
+++ backend/Application/EventHandlers/PersonDeletedEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Events;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace CleanArchitecture.Application.TodoItems.EventHandlers;
+
+public class PersonDeletedEventHandler : INotificationHandler<PersonCreatedEvent>
+{
+    private readonly ILogger<PersonCreatedEventHandler> _logger;
+
+    public PersonDeletedEventHandler(ILogger<PersonCreatedEventHandler> logger)
+    {
+        _logger = logger;
+    }
+
+    public Task Handle(PersonCreatedEvent notification, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Hahn Domain Event: {DomainEvent}", notification.GetType().Name);
+
+        return Task.CompletedTask;
+    }
+
+}
